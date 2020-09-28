c9c1e25bf7223fbd68abd8a1a44b8c998a1daff0
Alun Turner <alun.turner@googlemail.com>
2020-09-28 09:44:21 +0100

    18 mins in

--- sketch.js
+++ sketch.js
@@ -2,13 +2,17 @@ const flock = [];
 
 function setup() {
   createCanvas(640, 360);
-  flock.push(new Boid());
+  for (let i = 0; i < 100; i++) {
+    flock.push(new Boid());
+  }
 }
 
 function draw() {
   background(51);
 
   for (let boid of flock) {
+    boid.flock(flock);
+    boid.update();
     boid.show();
   }
 }
