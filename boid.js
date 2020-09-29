c7bf5a33fe6834b249b36283f791ef0d3504c4d7
Alun Turner <alun.turner@googlemail.com>
2020-09-29 08:43:38 +0100

    compelte cohesion

--- boid.js
+++ boid.js
@@ -4,7 +4,21 @@ class Boid {
     this.velocity = p5.Vector.random2D();
     this.velocity.setMag(random(2, 4));
     this.acceleration = createVector();
-    this.maxForce = 1;
+    this.maxSpeed = 4;
+    this.maxForce = 0.2;
+  }
+
+  edges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height;
+    }
   }
 
   align(boids) {
@@ -25,6 +39,33 @@ class Boid {
     }
     if (total > 0) {
       steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  cohesion(boids) {
+    let perceptionRadius = 50;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other !== this && d < perceptionRadius) {
+        steering.add(other.position);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.position);
+      steering.setMag(this.maxSpeed);
       steering.sub(this.velocity);
       steering.limit(this.maxForce);
     }
@@ -33,7 +74,10 @@ class Boid {
 
   flock(boids) {
     let alignment = this.align(boids);
-    this.acceleration = alignment;
+    this.acceleration.add(alignment);
+
+    let cohesion = this.cohesion(boids);
+    this.acceleration.add(cohesion);
   }
 
   show() {
@@ -45,5 +89,7 @@ class Boid {
   update() {
     this.position.add(this.velocity);
     this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0);
   }
 }
